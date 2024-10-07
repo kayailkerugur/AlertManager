33b52cf62f8e712805bfd060e59978e2b14f69b6
Ilker Kaya <kayailkerugur@gmail.com>
2024-10-08 01:20:15 +0300

    Initial Commit

---
+++ AlertManager/AlertManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlertManagerApp.swift
+//  AlertManager
+//
+//  Created by İlker Kaya on 8.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct AlertManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlertManager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AlertManager
+//
+//  Created by İlker Kaya on 8.10.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
